b55638a408dd5d1e648c6ac1246d01bd3e34da0c
jorola0411 <jorola@my.bcit.ca>
2024-08-06 17:42:35 -0700

    August 6 revision
    added parallax, quiz plugins, more css and more fonts

--- js/main.js
+++ js/main.js
@@ -1,8 +1,214 @@
 
+/* Slick Slider*/
+$(document).ready(function(){
+    $('.galleryslider').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          },
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
+  });
+
+/*Parallax*/
+  $(".smtparallax").parallax({
+    imageSrc: "images/smtparallax.jpg"
+
+});
+
+/* Quiz Section*/
+(function() {
+    var questions = [{
+      question: "Who is the main character of Shin Megami Tensei V?",
+      choices: ["Demi-Fiend","Nanashi","Cloud Strife","Yu Narukami","Nahobino"],
+      correctAnswer: 4
+    }, {
+      question: "When did Shin Megami Tensei IV: Apocalypse release in America?",
+      choices: ["September 20, 2016","Feburary 10, 2016","December 2, 2016","December 9, 2016","April 11, 2016"],
+      correctAnswer: 0
+    }, {
+      question: "Which one of these series are NOT a subseries in SMT? ",
+      choices: ["Devil Survivor","Catherine","Digital Devil Saga","Persona","Soul Hackers"],
+      correctAnswer: 1
+    }, {
+      question: "Shin Megami Tensei is owned by what video game company?",
+      choices: ["Bandai Namco","Nintendo","Atlus","FromSoftware","Rockstar Games"],
+      correctAnswer: 2
+    }, {
+      question: "Which of these are NOT a demon in SMT?",
+      choices: ["Manananggal","Nekomata","Beezlebub","Zeugle","Jack Frost"],
+      correctAnswer: 3
+    }];
+    
+    var questionCounter = 0; //Tracks question number
+    var selections = []; //Array containing user choices
+    var quiz = $('#quiz'); //Quiz div object
+    
+    // Display initial question
+    displayNext();
+    
+    // Click handler for the 'next' button
+    $('#next').on('click', function (e) {
+      e.preventDefault();
+      
+      // Suspend click listener during fade animation
+      if(quiz.is(':animated')) {        
+        return false;
+      }
+      choose();
+      
+      // If no user selection, progress is stopped
+      if (isNaN(selections[questionCounter])) {
+        alert('Please make a selection!');
+      } else {
+        questionCounter++;
+        displayNext();
+      }
+    });
+    
+    // Click handler for the 'prev' button
+    $('#prev').on('click', function (e) {
+      e.preventDefault();
+      
+      if(quiz.is(':animated')) {
+        return false;
+      }
+      choose();
+      questionCounter--;
+      displayNext();
+    });
+    
+    // Click handler for the 'Start Over' button
+    $('#start').on('click', function (e) {
+      e.preventDefault();
+      
+      if(quiz.is(':animated')) {
+        return false;
+      }
+      questionCounter = 0;
+      selections = [];
+      displayNext();
+      $('#start').hide();
+    });
+    
+    // Animates buttons on hover
+    $('.button').on('mouseenter', function () {
+      $(this).addClass('active');
+    });
+    $('.button').on('mouseleave', function () {
+      $(this).removeClass('active');
+    });
+    
+    // Creates and returns the div that contains the questions and 
+    // the answer selections
+    function createQuestionElement(index) {
+      var qElement = $('<div>', {
+        id: 'question'
+      });
+      
+      var header = $('<h2>Question ' + (index + 1) + ':</h2>');
+      qElement.append(header);
+      
+      var question = $('<p>').append(questions[index].question);
+      qElement.append(question);
+      
+      var radioButtons = createRadios(index);
+      qElement.append(radioButtons);
+      
+      return qElement;
+    }
+    
+    // Creates a list of the answer choices as radio inputs
+    function createRadios(index) {
+      var radioList = $('<ul>');
+      var item;
+      var input = '';
+      for (var i = 0; i < questions[index].choices.length; i++) {
+        item = $('<li>');
+        input = '<input type="radio" name="answer" value=' + i + ' />';
+        input += questions[index].choices[i];
+        item.append(input);
+        radioList.append(item);
+      }
+      return radioList;
+    }
+    
+    // Reads the user selection and pushes the value to an array
+    function choose() {
+      selections[questionCounter] = +$('input[name="answer"]:checked').val();
+    }
+    
+    // Displays next requested element
+    function displayNext() {
+      quiz.fadeOut(function() {
+        $('#question').remove();
+        
+        if(questionCounter < questions.length){
+          var nextQuestion = createQuestionElement(questionCounter);
+          quiz.append(nextQuestion).fadeIn();
+          if (!(isNaN(selections[questionCounter]))) {
+            $('input[value='+selections[questionCounter]+']').prop('checked', true);
+          }
+          
+          // Controls display of 'prev' button
+          if(questionCounter === 1){
+            $('#prev').show();
+          } else if(questionCounter === 0){
+            
+            $('#prev').hide();
+            $('#next').show();
+          }
+        }else {
+          var scoreElem = displayScore();
+          quiz.append(scoreElem).fadeIn();
+          $('#next').hide();
+          $('#prev').hide();
+          $('#start').show();
+        }
+      });
+    }
+    
+    // Computes score and returns a paragraph element to be displayed
+    function displayScore() {
+      var score = $('<p>',{id: 'question'});
+      
+      var numCorrect = 0;
+      for (var i = 0; i < selections.length; i++) {
+        if (selections[i] === questions[i].correctAnswer) {
+          numCorrect++;
+        }
+      }
+      
+      score.append('You got ' + numCorrect + ' out of ' +
+                   questions.length + ' correct!');
+      return score;
+    }
+  })();
+
+/*Lightbox gallery*/
 lightbox.option({
     'resizeDuration': 200,
     'wrapAround': true
 })
-
-
 
