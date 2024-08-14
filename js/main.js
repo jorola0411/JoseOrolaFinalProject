29e0fbd7ce32d8e4c49ac7d4d96e933d85038c50
jorola0411 <jorola@my.bcit.ca>
2024-08-14 13:26:12 -0700

    comments

--- js/main.js
+++ js/main.js
@@ -27,14 +27,9 @@ $(document).ready(function(){
               slidesToScroll: 1
             }
           },
-          // You can unslick at a given breakpoint now by adding:
-          // settings: "unslick"
-          // instead of a settings object
         ]
       });
   });
-
-/*Parallax*/
 
 /* Quiz Section*/
 (function() {
